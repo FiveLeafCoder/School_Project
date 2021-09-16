86a031d3bf82d69c11a2406b275a56a4a43be63a
FiveLeafCoder <90576397+FiveLeafCoder@users.noreply.github.com>
2021-09-16 10:58:18 -0400

    Create RoShamBow.py

---
+++ RoShamBow.py
@@ -0,0 +1,11 @@
+import random
+
+Welcome:
+  print("""
+  Greetings player,
+  
+  Hope you like playing Rock-Paper-Scissors, because that is all we do here.
+  You will be going against the computer.
+  
+  Best of luck player!""")
+  
